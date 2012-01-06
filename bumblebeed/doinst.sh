51ab21d8c9b57ce644f190b815f8861d51b3ef54
jgeboski <jgeboski@gmail.com>
2012-01-06 13:13:03 -0500

    bumblebeed: preserve existing configuration

---
+++ bumblebeed/doinst.sh
@@ -0,0 +1,26 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+preserve_perms etc/rc.d/rc.bumblebeed.new
+
+config etc/bumblebee/bumblebee.conf.new
+config etc/bumblebee/xorg.conf.nouveau.new
+config etc/bumblebee/xorg.conf.nvidia.well
