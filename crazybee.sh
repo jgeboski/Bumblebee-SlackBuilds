c49e52328357d3bcf09f1df882973c77e480d5b5
Neeraj Rajesh <10393055+nrj5k@users.noreply.github.com>
2020-12-20 11:38:25 -0600

    Update crazybee.sh
    removed email in my local version accidentally, added it back

--- crazybee.sh
+++ crazybee.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ## run as root
 
-# Ryan P. C. McQuen | Everett, WA
+# Ryan P. C. McQuen | Everett, WA | ryanpcmcquen@member.fsf.org
 
 # curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/slackware/crazybee.sh | sh
 
